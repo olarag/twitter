049599f9b2b634a0439830229208da5e543a4431
olarag <oscar@difusion.mx>
2022-04-23 22:11:12 -0500

    Se agrego requerimiento de cambio de username

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -11,13 +11,18 @@ describe("Test for UserService", () => {
 	})
 
 	test("2. Get all user data in a list", () =>{
-		const user2 = UserService.create(1, "olarag", "Oscar")
-		const userInfoInList = UserService.getInfo(user2)
+		const user = UserService.create(1, "olarag", "Oscar")
+		const userInfoInList = UserService.getInfo(user)
 		expect(userInfoInList[0]).toBe(1)
 		expect(userInfoInList[1]).toBe("olarag")
 		expect(userInfoInList[2]).toBe("Oscar")
 		expect(userInfoInList[3]).toBe("Sin bio")
 	})
 
+	test("3. Update username", () => {
+		const user = UserService.create(1, "olarag", "Oscar")
+		UserService.updateUserUsername(user, "olara")
+		expect(user.username).toBe("olara")
+	})
 
 })
