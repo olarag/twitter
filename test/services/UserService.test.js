1c19f45610dc09ce9c77c2984f196c3cd211ceed
olarag <oscar@difusion.mx>
2022-04-23 19:17:32 -0500

    se agregó requerimiento de test

---
+++ test/services/UserService.test.js
@@ -0,0 +1,13 @@
+cont UserService = require('./../../app/services/UserService')
+
+describe("Test for UserService", () => {
+
+	test("1. Create a new user using the UserService", () =>{
+		const user = UserService.create(1, "olarag", "Oscar")
+		expect(user.username).toBe("olarag")
+		expect(user.name).toBe("Oscar")
+		expect(user.id).toBe(1)
+		expect(user.bio).not.toBeUndefined()
+	})
+
+})
