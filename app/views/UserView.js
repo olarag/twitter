a7b0fe81b1b9b311821a295dcda68c4e29545896
olarag <oscar@difusion.mx>
2022-05-09 19:41:07 -0500

    modificación de código

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -2,14 +2,14 @@ const UserService = require('./../services/UserService')
 
 class UserView{
 
-	static createUser(playload){
+	static createUser(payload){
 		if(payload === null){
 			console.log("Error es null")
 			return {error: "El payload no existe."}
 		} else if(typeof payload.username === 'string' && typeof payload.name === 'string' && typeof payload.id === 'number') {
 			return UserService.create(payload.id, payload.username, payload.name)
 		} else {
-			return {error: "Error, las propiedades del playload necesitan tener un valor válido"}
+			return {error: "Error, las propiedades del payload necesitan tener un valor válido"}
 		}
 	}
 
