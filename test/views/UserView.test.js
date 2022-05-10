40cde1d238d6eabe1aa4ee6cd7273bd9a2b2c553
olarag <oscar@difusion.mx>
2022-05-09 19:41:40 -0500

    se ejecutaron las pruebas correctamente

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -11,18 +11,18 @@ describe("Test for UserView", () => {
 	test("Return an error object when try to create a new user with a payload with invalid properties", () => {
 		const payload = {username: null, name: 12, id : "id"}
 		const result = UserView.createUser(payload)
-		expect(resut.error).toMatch(/necesitan tener un valor válido/)
+		expect(result.error).toMatch(/necesitan tener un valor válido/)
 	})
 
 	test("Return an error object when try to create a new user with a payload with missing properties", () => {
 		const payload = {username: "Username"}
 		const result = UserView.createUser(payload)
-		expect(result.error).toMatch(/necesitan tener un valir válido/)
+		expect(result.error).toMatch(/Error, las propiedades del payload necesitan tener un valor válido/)
 	})
 
 	test("Create a user by a given valid payload", () => {
-		const payload = {usrname: "username", id: 1, name: "name"}
-		const result = UserView.createUSer(payload)
+		const payload = {username: "username", id: 1, name: "name"}
+		const result = UserView.createUser(payload)
 		expect(result.name).toBe("name")
 		expect(result.username).toBe("username")
 		expect(result.id).toBe(1)
